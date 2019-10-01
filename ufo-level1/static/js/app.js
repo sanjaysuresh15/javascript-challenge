5810da68d31e55ced96d0365a40f99a333c9217b
Sanjay Suresh/JAM/Brenntag <sanjaysuresh15@gmail.com>
2019-10-01 09:56:15 -0500

    add notes

--- ufo-level1/static/js/app.js
+++ ufo-level1/static/js/app.js
@@ -6,11 +6,6 @@
  let tbody = d3.select("tbody");
 
 
-
-
-
-
-
    // loop through the data and append rows with UFO data
    data.forEach(function(UFO) {
       console.log(UFO);
@@ -21,10 +16,11 @@
         cell.text(value);
       });
     }); 
-
+// select the filter button to be where you click to filter
 let button = d3.select("#filter-btn");
 
 button.on("click", function() {
+  // clear the field for only the input value
   tbody.html("");
       // Select the input element and get the raw HTML node
   let inputElement = d3.select("#datetime");
@@ -32,11 +28,13 @@ button.on("click", function() {
       // Get the value property of the input element
   let inputValue = inputElement.property("value");
     
+
+  // print the data value that you put in
     console.log(inputValue);
     console.log(data);
-    
+    // filter the datetime field for the input value
   let filteredData = data.filter(data => data.datetime === inputValue);
-   
+  //  run through the data for only the filtered value and print the rows in the table body
   filteredData.forEach(function(UFO) {
     console.log(UFO);
     let row = tbody.append("tr");
